cd3d5c25cb3c315c85cecdf3de4bf82373a4b763
Xin <xin@imfing.com>
2022-11-21 23:36:35 +0000

    Add code to get list of issues

--- main.ts
+++ main.ts
@@ -2,14 +2,24 @@ import { Octokit } from "https://cdn.skypack.dev/octokit?dts";
 
 // Get GitHub token from environment variable
 const GITHUB_TOKEN = Deno.env.get("GITHUB_TOKEN")!;
-if (!GITHUB_TOKEN) {
-  console.error("GITHUB_TOKEN is not set...");
-  Deno.exit(1);
-}
+const GITHUB_REPOSITORY = Deno.env.get("GITHUB_REPOSITORY")
+
+const [owner, repo] = GITHUB_REPOSITORY.split("/")
 
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
-const { data: { login } } = await octokit.rest.users.getAuthenticated();
 
-console.log("Hello, %s", login);
+// Iterate over all issues
+const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
+  owner: owner,
+  repo: repo,
+  per_page: 100,
+  state: "all", // TODO: make it a flag
+});
+
+for await (const { data: issues } of iterator) {
+  for (const issue of issues) {
+    console.log("Issue #%d: %s", issue.number, issue.title);
+  }
+}
 
 Deno.exit();
